842a78d9b710334da1ded46178f78e69c7cc64a8
gusmmm <jgustavomartins@gmail.com>
2025-09-07 19:55:48 +0100

    Update API request example and modify 'sexo' field in doente creation

--- src/api.py
+++ src/api.py
@@ -5,7 +5,7 @@ from icecream import ic
 
 from src.db import init_db, get_session
 from src.models.models import SexoEnum, Doente, DoenteCreate, Internamento
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,20 +15,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-doentes = [
-    {"id": 1, "nome": "Alice", "numero_processo": 30,
-     "data_nascimento": "1990-01-01", "sexo": "F", "morada": "Rua A, 123"},
-    {"id": 2, "nome": "Bob", "numero_processo": 45,
-     "data_nascimento": "1985-05-12", "sexo": "M", "morada": "Rua B, 456"},
-    {"id": 3, "nome": "Charlie", "numero_processo": 22,
-     "data_nascimento": "1978-09-23", "sexo": "M", "morada": "Rua C, 789"},
-    {"id": 4, "nome": "Diana", "numero_processo": 15,
-     "data_nascimento": "1992-11-30", "sexo": "F", "morada": "Rua D, 101"},
-    {"id": 5, "nome": "Eve", "numero_processo": 50,
-     "data_nascimento": "1988-07-14", "sexo": "F", "morada": "Rua E, 202"},
-]
 
-"""
 @app.get("/")
 async def index() -> dict[str, str]:
     return {"message": "Hello, World!"}
@@ -40,46 +27,38 @@ async def about() -> dict[str, str]:
 
 
 @app.get("/doentes")
-async def read_doentes(sexo: SexoEnum | None = None) -> list[DoenteWithID]:
+async def read_doentes(
+    sexo: SexoEnum | None = None,
+    session: Session = Depends(get_session)
+) -> list[Doente]:
+    statement = select(Doente)
     if sexo:
-        result = [doente for doente in doentes
-              if doente["sexo"].upper() == sexo.value.upper()]
-        if not result:
-            raise HTTPException(
-                status_code=404,
-                detail="No doentes found with the specified sexo"
-            )
-        return [DoenteWithID(**doente) for doente in result]
-    return [
-        DoenteWithID(**doente) for doente in doentes
-    ]
-
-
-@app.get("/doentes/{doente_id}")
-async def read_doente(doente_id: int) -> DoenteWithID:
-    for doente in doentes:
-        if doente["id"] == doente_id:
-            return DoenteWithID(**doente)
-    raise HTTPException(status_code=404, detail="Doente not found")
+        statement = statement.where(Doente.sexo == sexo)
+    doentes = session.exec(statement).all()
+    return list(doentes)
 
 
 @app.get("/doentes/numero_processo/{numero_processo}")
-async def read_doente_by_numero_processo(numero_processo: int) -> DoenteWithID:
-    for doente in doentes:
-        if doente["numero_processo"] == numero_processo:
-            return DoenteWithID(**doente)
-    raise HTTPException(status_code=404, detail="Doente not found")
+async def read_doente_by_numero_processo(
+    numero_processo: int,
+    session: Session = Depends(get_session)
+) -> Doente:
+    statement = select(Doente).where(Doente.numero_processo == numero_processo)
+    doente = session.exec(statement).first()
+    if not doente:
+        raise HTTPException(status_code=404, detail="Doente not found")
+    return doente
 
 
-@app.get("/doentes/sexo/{sexo}")
-async def read_doentes_by_sexo(sexo: SexoEnum) -> list[Doente]:
-    result = [doente for doente in doentes
-              if doente["sexo"].upper() == sexo.value.upper()]
-    if not result:
-        raise HTTPException(status_code=404,
-                            detail="No doentes found with the specified sexo")
-    return [Doente(**doente) for doente in result]
-"""
+# @app.get("/doentes/sexo/{sexo}")
+# async def read_doentes_by_sexo(sexo: SexoEnum) -> list[Doente]:
+#     result = [doente for doente in doentes
+#               if doente["sexo"].upper() == sexo.value.upper()]
+#     if not result:
+#         raise HTTPException(status_code=404,
+#                             detail="No doentes found with the specified sexo")
+#     return [Doente(**doente) for doente in result]
+
 
 @app.post("/doentes", status_code=201)
 async def create_doente(
